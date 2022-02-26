208eb22267c518d11b89b3e66ba181ea8aa7fdf0
tkadziolka <tmax0135@gmail.com>
2022-02-26 21:50:55 +0100

    Added base domain models

---
+++ ElectroShopApi/Domain/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using ElectroShopApi.Domain;
+
+namespace ElectroShopApi
+{
+    public class Cart
+    {
+        public List<Product> FirstName { get; init; }
+    }
+}
---
+++ ElectroShopApi/Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ElectroShopApi.Domain
+{
+    public record Product
+    {
+        public Guid Guid { get; init; }
+        public string Name { get; init; }
+        public ProductCategory Category { get; init; }
+    }
+}
