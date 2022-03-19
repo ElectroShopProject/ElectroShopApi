86f168a4c9dd167c174f76325c003657d1ad16e6
tkadziolka <tmax0135@gmail.com>
2022-03-19 21:21:23 +0100

    Refactored code to minimal API framework

---
+++ ElectroShopApi/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectroShopApi.Controllers
+{
+    [Route("products")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Printer", "Computer", "Phone"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Summaries;
+        }
+    }
+}
