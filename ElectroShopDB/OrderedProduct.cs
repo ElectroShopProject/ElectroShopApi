f6c4aa31812e38ce11fa5e7587a2ae9649f5514a
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2022-05-01 16:03:02 +0200

    Created tables for order

---
+++ ElectroShopDB/OrderedProduct.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectroShopDB
+{
+    public class OrderedProduct
+    {
+        [Key]
+        public string OrderId { get; set; }
+        [Key]
+        public string ProductId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
---
+++ ElectroShopDB/Tables/Order/OrderTable.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectroShopDB
+{
+    public class OrderTable
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string CartId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string PaymentId { get; set; }
+    }
+}
